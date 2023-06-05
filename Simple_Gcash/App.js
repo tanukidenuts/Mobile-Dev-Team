f3ecc36687fb3939c7f26e87b59a941c55ef32f9
Neds001 <NedsMamitag001@gmail.com>
2023-06-05 14:19:00 +0800

    Update UI upper

--- Simple_Gcash/App.js
+++ Simple_Gcash/App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, 
+        Text, 
+        View, 
+        Image,
+        Platform,
+        SafeAreaView,
+       } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style = {{
+      backgroundColor: '#fff',
+      flex: 1,
+      flexDirection: 'column',
+      paddingTop: Platform.OS === "android" ? StatusBar.currentHeigt : 0,
+    }}>
+      <View style = {{backgroundColor: 'dodgerblue', flex: .2,}}>
+        <View style = {{flexDirection: 'row', paddingTop: 40, justifyContent: 'center'}}>
+        <Image source={require('./assets/GCASH-Logo.png')}
+             style={{width: 40, height: 40}}
+      />
+      <Text style = {styles.gcash}>GCash</Text>
+        </View>
+      
+      </View>
+
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  logo: {
+    width: '50%',
+    height: '10%',
   },
+  gcash: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 40
+  },
+  
+ 
 });
