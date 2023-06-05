32d272e4e14fdfc51c34a295c2077feee562a786
Neds001 <NedsMamitag001@gmail.com>
2023-06-05 21:48:52 +0800

    Update Layout

--- Simple_Gcash/App.js
+++ Simple_Gcash/App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import {StyleSheet, 
         Text, 
         View, 
         Image,
         Platform,
-        SafeAreaView, TouchableOpacity
+        StatusBar,
+        SafeAreaView, 
+        TouchableOpacity,
        } from 'react-native';
 
 export default function App() {
@@ -12,43 +13,52 @@ export default function App() {
     <SafeAreaView style = {{
       backgroundColor: '#fff',
       flex: 1,
-      flexDirection: 'column',
       paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     }}>
-      <View style = {{backgroundColor: 'dodgerblue', flex: .35,}}>
-        <View style = {{flexDirection: 'row', paddingTop: 40, justifyContent: 'center'}}>
-        <Image source={require('./assets/GCASH-Logo.png')}
+      {/*Blue Box View*/}
+      <View style = {{backgroundColor: 'dodgerblue', flex: .30,}}>
+      
+        {/*Logo and Text*/}
+        <View style = {{flexDirection: 'row', paddingTop: 10, justifyContent: "center"}}>
+          <Image source={require('./assets/GCASH-Logo.png')}
              style={{width: 40, height: 40}}
-      />
-      
-      <Text style = {styles.gcash}>GCash</Text>
-      
+        />
+          <Text style = {styles.gcash}>GCash</Text>
         </View>
-        
+
+        {/*User*/}
         <View style = {{flexDirection: 'row', paddingTop: 40, justifyContent: 'center', left: 150, bottom: 85}}>
-        <TouchableOpacity>
-        <Image source ={require("./assets/user.png")}/>
-      </TouchableOpacity>
-      </View>
-      <View style = {{flexDirection: 'row', paddingTop: 40, justifyContent: 'center', right: 150, bottom: 160}}>
-        <TouchableOpacity>
-        <Image source ={require("./assets/menu.png")}/>
-      </TouchableOpacity>
-      </View>
-      <View style = {{flexDirection: 'row', paddingTop: 40, justifyContent: 'center', right: 150, bottom: 10}}>
-        <TouchableOpacity>
-        <Image source ={require("./assets/Send.png")}/>
-      </TouchableOpacity>
-      </View>
+          <TouchableOpacity>
+            <Image source ={require("./assets/user.png")}/>
+          </TouchableOpacity>
+        </View>
+
+        {/*Menu*/}
+        <View style = {{flexDirection: 'row', paddingTop: 40, justifyContent: 'center', right: 150, bottom: 165}}>
+          <TouchableOpacity>
+            <Image source ={require("./assets/menu.png")}/>
+          </TouchableOpacity>
+        </View>
+
+        {/*Icons*/}
+        <View style = {{flexDirection: 'row', paddingTop: 40, justifyContent: 'center', right: 150, bottom: 10}}>
+          <TouchableOpacity>
+            <Image source ={require("./assets/Send.png")}/>
+          </TouchableOpacity>
+        </View>
+
       <Text style = {styles.balance}>Balance</Text>
       <Text style = {styles.balanceNumber}>₱ 200,000.69</Text>
+
       </View>
-
-
     </SafeAreaView>
     
   );
+  
 }
+
+
+
 
 const styles = StyleSheet.create({
   logo: {
@@ -64,13 +74,9 @@ const styles = StyleSheet.create({
   balance: {
     color: 'white',
     fontWeight: 'bold',
-    fontSize: 35,
-    marginLeft: 0.45,
-    left: 145,
-    bottom: 220
-    
-    
-    
+    fontSize: 15,
+    marginLeft: '42%',
+    bottom: 220,
   },
   balanceNumber: {
     color: 'white',
@@ -78,10 +84,7 @@ const styles = StyleSheet.create({
     fontSize: 50,
     marginLeft: -110,
     left: 160,
-    bottom: 170
-    
-    
-    
+    bottom: 185,
   },
  
 });
