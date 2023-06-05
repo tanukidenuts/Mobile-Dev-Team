106a3b1df9fed27fda796ee78d9d80f18db2d7af
Tiantian42 <quisoramister@gmail.com>
2023-06-05 17:58:16 +0800

    correct the spelling of StatusBar.currentHeight

--- Simple_Gcash/App.js
+++ Simple_Gcash/App.js
@@ -1,87 +1,91 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, 
-        Text, 
-        View, 
-        Image,
-        Platform,
-        SafeAreaView, TouchableOpacity
-       } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Platform,
+  SafeAreaView,
+  TouchableOpacity
+} from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style = {{
-      backgroundColor: '#fff',
+    <SafeAreaView style={{
       flex: 1,
-      flexDirection: 'column',
-      paddingTop: Platform.OS === "android" ? StatusBar.currentHeigt : 0,
+      backgroundColor: '#fff',
+      paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     }}>
-      <View style = {{backgroundColor: 'dodgerblue', flex: .35,}}>
-        <View style = {{flexDirection: 'row', paddingTop: 40, justifyContent: 'center'}}>
-        <Image source={require('./assets/GCASH-Logo.png')}
-             style={{width: 40, height: 40}}
-      />
-      
-      <Text style = {styles.gcash}>GCash</Text>
-      
+      <View style={{ backgroundColor: 'dodgerblue', flex: 0.35 }}>
+        <View style={styles.header}>
+          <Image
+            source={require('./assets/GCASH-Logo.png')}
+            style={styles.logo}
+          />
+          <Text style={styles.gcash}>GCash</Text>
         </View>
-        
-        <View style = {{flexDirection: 'row', paddingTop: 40, justifyContent: 'center', left: 150, bottom: 85}}>
-        <TouchableOpacity>
-        <Image source ={require("./assets/user.png")}/>
-      </TouchableOpacity>
-      </View>
-      <View style = {{flexDirection: 'row', paddingTop: 40, justifyContent: 'center', right: 150, bottom: 160}}>
-        <TouchableOpacity>
-        <Image source ={require("./assets/menu.png")}/>
-      </TouchableOpacity>
-      </View>
-      <View style = {{flexDirection: 'row', paddingTop: 40, justifyContent: 'center', right: 150, bottom: 10}}>
-        <TouchableOpacity>
-        <Image source ={require("./assets/Send.png")}/>
-      </TouchableOpacity>
-      </View>
-      <Text style = {styles.balance}>Balance</Text>
-      <Text style = {styles.balanceNumber}>₱ 200,000.69</Text>
-      </View>
 
+        <View style={styles.iconsContainer}>
+          <TouchableOpacity>
+            <Image source={require('./assets/user.png')} />
+          </TouchableOpacity>
 
+          <TouchableOpacity>
+            <Image source={require('./assets/menu.png')} />
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Image source={require('./assets/Send.png')} />
+          </TouchableOpacity>
+        </View>
+
+        <Text style={styles.balance}>Balance</Text>
+        <Text style={styles.balanceNumber}>₱ 200,000.69</Text>
+      </View>
     </SafeAreaView>
-    
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    paddingTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   logo: {
-    width: '50%',
-    height: '5%',
+    width: 40,
+    height: 40,
   },
   gcash: {
     color: 'white',
     fontWeight: 'bold',
     fontSize: 40,
-    marginLeft: 10
+    marginLeft: 10,
+  },
+  iconsContainer: {
+    flexDirection: 'row',
+    paddingTop: 40,
+    justifyContent: 'center',
   },
   balance: {
     color: 'white',
     fontWeight: 'bold',
     fontSize: 20,
     marginLeft: 10,
-    left: 145,
-    bottom: 220
-    
-    
-    
+    position: 'absolute',
+    bottom: 220,
+    left: '50%',
+    transform: [{ translateX: -40 }],
   },
   balanceNumber: {
     color: 'white',
     fontWeight: 'bold',
     fontSize: 50,
-    marginLeft: -110,
-    left: 160,
-    bottom: 170
-    
-    
-    
+    position: 'absolute',
+    bottom: 170,
+    left: '50%',
+    transform: [{ translateX: -110 }],
   },
- 
 });
