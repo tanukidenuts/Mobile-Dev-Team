2734f0b2403d5621c0a0d01fac30506590779629
Tiantian42 <quisoramister@gmail.com>
2023-06-06 20:53:32 +0800

    display balance in home

--- drawer-navigation/screens/Home.js
+++ drawer-navigation/screens/Home.js
@@ -1,10 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
 
 export default function Home() {
   return (
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.balanceContainer}>
+        <Text style={styles.balance}>Balance</Text>
+        <Text style={styles.balanceNumber}>₱69</Text>
+      </View>
     </View>
-  )
+  );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  balanceContainer: {
+    backgroundColor: 'orange',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  balance: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  balanceNumber: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 50,
+  },
+});
